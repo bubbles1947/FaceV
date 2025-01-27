0f7d11d1ed2300b496c3e1da196e5ee73e076eb0
bubbles1947 <132488366+bubbles1947@users.noreply.github.com>
2025-01-27 22:07:45 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,9 @@
 <?php
-// logout.php
-
 session_start();
 
-// Destroy the session to log the user out
 session_unset();
 session_destroy();
 
-// Redirect to login page
 header("Location: login.php");
 exit();
 ?>
