10e6e974f7888d2c2e68226b7198cb4df76b6987
bubbles1947 <132488366+bubbles1947@users.noreply.github.com>
2025-01-27 22:09:07 +0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,16 +1,10 @@
 <?php
-// profile.php
-
 session_start();
 
-// Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
-    // If not logged in, redirect to login page
     header("Location: login.php");
     exit();
 }
-
-// Get user information from the session
 $email = $_SESSION['email'];
 
 ?>
